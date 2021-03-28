f2cd06d045b2126fb7b82cbd0ca2ffb45c76f7c4
Anthony Layne <vanheuson@gmail.com>
2021-03-28 11:09:14 -0400

    simplify theme object

--- Sources/ArtWalkCore/APIModels/Theme.swift
+++ Sources/ArtWalkCore/APIModels/Theme.swift
@@ -1,96 +1,11 @@
 
 import Foundation
-import SwiftUI
 
 public struct Theme: Codable {
     
-    private(set) var color: Color
+    private var hex: String
     
-    public init?(withHexColor hex: String) {
-        let r, g, b, a: CGFloat
-        
-        if hex.hasPrefix("#") {
-            let start = hex.index(hex.startIndex, offsetBy: 1)
-            let hexColor = String(hex[start...])
-            
-            if hexColor.count == 8 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-                
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-                    
-                    self.color = Color(red: Double(r), green: Double(g), blue: Double(b), opacity: Double(a))
-                }
-            }
-        }
-        
-        return nil
-    }
-}
-
-@available(OSX 11.0, *)
-fileprivate extension Color {
-    #if os(macOS)
-    typealias SystemColor = NSColor
-    #else
-    typealias SystemColor = UIColor
-    #endif
-    
-    var colorComponents: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)? {
-        var r: CGFloat = 0
-        var g: CGFloat = 0
-        var b: CGFloat = 0
-        var a: CGFloat = 0
-        
-        #if os(macOS)
-        SystemColor(self).getRed(&r, green: &g, blue: &b, alpha: &a)
-        // Note that non RGB color will raise an exception, that I don't now how to catch because it is an Objc exception.
-        #else
-        guard SystemColor(self).getRed(&r, green: &g, blue: &b, alpha: &a) else {
-            // Pay attention that the color should be convertible into RGB format
-            // Colors using hue, saturation and brightness won't work
-            return nil
-        }
-        #endif
-        
-        return (r, g, b, a)
-    }
-}
-
-extension Color: Codable {
-    enum CodingKeys: String, CodingKey {
-        case red, green, blue, alpha
-    }
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let r = try container.decode(Double.self, forKey: .red)
-        let g = try container.decode(Double.self, forKey: .green)
-        let b = try container.decode(Double.self, forKey: .blue)
-        let a = try container.decode(Double.self, forKey: .alpha)
-        
-        self.init(red: r, green: g, blue: b, opacity: a)
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        if #available(OSX 11.0, *) {
-            guard let colorComponents = self.colorComponents else {
-                return
-            }
-            
-            var container = encoder.container(keyedBy: CodingKeys.self)
-            
-            try container.encode(colorComponents.red, forKey: .red)
-            try container.encode(colorComponents.green, forKey: .green)
-            try container.encode(colorComponents.blue, forKey: .blue)
-            try container.encode(colorComponents.blue, forKey: .alpha)
-            
-        } else {
-            // Fallback on earlier versions
-        }
+    public init(withHexColor hex: String) {
+        self.hex = hex
     }
 }
